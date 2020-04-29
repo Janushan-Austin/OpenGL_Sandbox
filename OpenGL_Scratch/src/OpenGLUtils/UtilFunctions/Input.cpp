9598a3835c5117307a012f03bb87096dd7e8e478
Janushan-Austin <38991541+Janushan-Austin@users.noreply.github.com>
2020-04-28 18:04:00 -0700

    Added a process input function to take a camera object

--- OpenGL_Scratch/src/OpenGLUtils/UtilFunctions/Input.cpp
+++ OpenGL_Scratch/src/OpenGLUtils/UtilFunctions/Input.cpp
@@ -20,3 +20,30 @@ void processCameraInput(GLFWwindow * window, glm::vec3& cameraPos, glm::vec3& ca
 		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed * deltaTime;
 	}
 }
+
+void processCameraInput(GLFWwindow* window, Camera& camera, float deltaTime) {
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+		camera.ProcessKeyboardInput(MovementDirection::FORWARD, deltaTime);
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+		camera.ProcessKeyboardInput(MovementDirection::BACKWARD, deltaTime);
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+		camera.ProcessKeyboardInput(MovementDirection::LEFT, deltaTime);
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+		camera.ProcessKeyboardInput(MovementDirection::RIGHT, deltaTime);
+	}
+	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS) {
+		camera.ProcessKeyboardInput(MovementDirection::ROLL_LEFT, deltaTime);
+	}
+	if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS) {
+		camera.ProcessKeyboardInput(MovementDirection::ROLL_RIGHT, deltaTime);
+	}
+	if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS) {
+		camera.ProcessKeyboardInput(MovementDirection::UP, deltaTime);
+	}
+	if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS) {
+		camera.ProcessKeyboardInput(MovementDirection::DOWN, deltaTime);
+	}
+}
