1edf9de57411c7dd1a4b8a54895b031dd7f58430
Janushan-Austin <38991541+Janushan-Austin@users.noreply.github.com>
2020-04-28 18:04:23 -0700

    Update Input.h

--- OpenGL_Scratch/src/OpenGLUtils/UtilFunctions/Input.h
+++ OpenGL_Scratch/src/OpenGLUtils/UtilFunctions/Input.h
@@ -2,4 +2,5 @@
 #include "OpenGLUtils/OpenGLUtils.h"
 
 void processInput(GLFWwindow* window);
+void processCameraInput(GLFWwindow* window, Camera& camera, float deltaTime);
 void processCameraInput(GLFWwindow * window, glm::vec3& cameraPos, glm::vec3& cameraFront, glm::vec3& cameraUp, float cameraSpeed = 0.5f, float deltaTime = 1.0f);
