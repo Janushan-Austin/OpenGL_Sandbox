3c6a13791f1fabf766641b6ad28da230db33648b
Janushan-Austin <38991541+Janushan-Austin@users.noreply.github.com>
2020-04-28 18:05:18 -0700

    Update CallBackFunctions.cpp

--- OpenGL_Scratch/src/OpenGLUtils/UtilFunctions/CallBackFunctions.cpp
+++ OpenGL_Scratch/src/OpenGLUtils/UtilFunctions/CallBackFunctions.cpp
@@ -1,6 +1,6 @@
 #include "CallBackFunctions.h"
 
-void framebuffer_size_callback(GLFWwindow* window, int newHeight, int newWidth) {
+void framebuffer_size_callback(GLFWwindow* window, int newWidth, int newHeight) {
 	glViewport(0, 0, newWidth, newHeight);
 }
 
